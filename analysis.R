69ab384a4d9795bc3a4691b41fe64bb7e9f1d20d
Maddie <>
2021-02-19 16:47:04 -0800

    code working, country reports made

--- analysis.R
+++ analysis.R
@@ -1,57 +1,8 @@
 # Analysis of UNHCR Data
 
-
-# Set Up ------------------------------------------------------------------
-
-library(tidyverse)
-library(countrycode)
-all_data <- read.csv("data/population.csv", skip = 14)
-
 # Simple exploration
 dim(all_data)
 unique(all_data$Year)
-length(unique(all_data$Country.of.origin))
-length(unique(all_data$Country.of.asylum))
 
-data <- all_data%>%
-  filter(Year == 2020)%>%
-  select(contains("Country"), Asylum.seekers)
-dim(data)
-
-country_of_interest <- "USA"
-
-country_name <- countrycode(country_of_interest, origin = 'iso3c', destination = 'country.name')
-
-country_data <- data%>%
+country_data <- data %>%
   filter(Country.of.asylum..ISO. == country_of_interest)
-
-
-# High Level Questions ----------------------------------------------------
-
-# From how many countries do asylum seekers come from (country of interest)?
-num_countries <- nrow(country_data)
-
-# How many people sought asylum in the country in 2020
-num_people <- country_data %>%
-  summarise(total_people = sum(Asylum.seekers)) %>%
-  pull()
-
-top_ten_countries <- country_data %>%
-  top_n(10, wt = Asylum.seekers)%>%
-  select(Country.of.origin, Asylum.seekers)
-
-
-# Make a map --------------------------------------------------------------
-shapefile <- map_data("world")
-
-# Get iso3 codes and join our data
-shapefile <- shapefile%>%
-  mutate(Country.of.origin..ISO. = countrycode(region, origin = "country.name", destination = 'iso3c')) %>%
-  left_join(country_data, by = "Country.of.origin..ISO.")
-
-asylum_map <- ggplot(data = shapefile) +
-  geom_polygon(
-    mapping = aes(x = long, y = lat, group = group, fill = Asylum.seekers)) +
-  labs(title = paste("Number of People Seeking Asylum in", country_name), 
-       x = "", y = "", fill = "Num. People") +
-  theme_minimal()
